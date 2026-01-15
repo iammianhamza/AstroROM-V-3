6643eb8d46838c5d65a516082c2a5eaa9e2364c1
iammianhamza <mianhamza001226@gmail.com>
2026-01-15 18:32:26 +0500

    Fix camera crashes and update configurations
    This script fixes crashes in Night Mode and Single Take Video Mode by updating necessary files.

---
+++ astro/platform/sd_8250/camera/fix-camera-mod.sh
@@ -0,0 +1,11 @@
+# Night Mode crash fix
+ADD_FROM_FW "stock" "system" "lib64/libSwIsp_wrapper_v1.camera.samsung.so"
+ADD_FROM_FW "stock" "system" "lib64/libSwIsp_core.camera.samsung.so" 
+
+# Fix Single Take Video Mode
+REMOVE "vendor" "etc/singletake"
+ADD_FROM_FW "dm3q" "vendor" "etc/singletake"
+FF "GALLERY_CONFIG_AI_EXPANSION" "AI_Timelapse"
+
+REMOVE "vendor" "saiv/swisp_1.0"
+ADD_FROM_FW "dm3q" "vendor" "saiv/swisp_1.0"
